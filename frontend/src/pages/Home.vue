07f374c2f2f885b95df8e57235012139015eee6f
david ducluzeaud <davidducluzeaud@macbook-pro-de-david.home>
2019-12-28 21:41:56 +0100

    json upload

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -1,13 +1,45 @@
 <template>
-  <div>
-    <p>Home</p>
-    <v-btn color="info">Info</v-btn>
+  <div class="wrapper">
+    <div class="container">
+      <input type="file" @change="handleJSONUpload"/>
+      <v-btn @click="submitJSON" :disabled="disabled">Soumettre</v-btn>
+    </div>
   </div>
 </template>
 
-<script>
+<script> 
+import { UPLOAD_TRANSLATIONS } from '@/graphql/file'
+
 export default {
-  name: "Home"
+  name: "Home",
+  data() {
+    return {
+      lang: '',
+      JSON: '',
+      loading: false
+    } 
+  },
+  methods: {
+    handleJSONUpload(e) {
+      const reader = new FileReader();
+      this.lang = e.target.files[0].name.split('.')[0]
+      reader.readAsText(e.target.files[0]);
+      reader.onload = e => {this.JSON = e.target.result};
+    },
+    async submitJSON() {
+      await this.$apollo.mutate({
+        mutation: UPLOAD_TRANSLATIONS,
+        variables: {
+          value: JSON.parse(this.JSON),
+        }
+      });
+    },
+  },
+  computed: {
+    disabled() {
+        return this.JSON === ""; // or === 0   
+    },
+}
 };
 </script>
 
