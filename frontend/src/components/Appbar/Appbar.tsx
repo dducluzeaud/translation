3cb8d921b47fe3221807f1c146bbd95d1599f59b
david ducluzeaud <davidducluzeaud@mbp-de-david.lan>
2020-01-07 00:04:57 +0100

    set appbar nav

--- frontend/src/components/Appbar/Appbar.tsx
+++ frontend/src/components/Appbar/Appbar.tsx
@@ -1,13 +1,13 @@
-import { Box, Anchor, Header } from "grommet";
-import React from "react";
+import { Box, Anchor, Header } from 'grommet'
+import React from 'react'
 
 const Appbar: React.FC = () => (
   <Header background="light-1" pad="small" elevation="small">
     <Box direction="row" gap="medium">
-      <Anchor label="Home" href="/home" />
-      <Anchor label="Profile" href="#" />
+      <Anchor label="Add" href="/translation/add" />
+      <Anchor label="List" href="/translations" />
     </Box>
   </Header>
-);
+)
 
-export default Appbar;
+export default Appbar
